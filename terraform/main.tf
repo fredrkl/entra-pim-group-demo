08bcafeb3e2bd45efdee61d475a511c9af955e1e
Fredrik Klingenberg <fredrkl@gmail.com>
2024-04-22 06:40:35 +0200

    ✨(terraform): Azure AD provider
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,20 @@ provider "azurerm" {
   features {}
 }
 
+provider "azuread" {
+  # Configuration options
+}
+
 terraform {
   required_version = ">= 1.7"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.85.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.48.0"
     }
   }
   backend "azurerm" {
