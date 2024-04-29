120c3ab96db375ecd98254e050916e2dbefbd281
Fredrik Klingenberg <fredrkl@gmail.com>
2024-04-29 07:23:41 +0200

    🐛(ps/assignEligibleGroup): Fix the assignment
    Closes: #28
    
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- ps/assignEligibleGroup.ps1
+++ ps/assignEligibleGroup.ps1
@@ -1,23 +1,22 @@
 Import-Module Microsoft.Graph.Identity.SignIns
 
 # Assign a group to `Eligibility` role for PIM Group
-$teamDemo = "1bef0223-af48-4cee-bd94-afb428f7259e" # team-demo
-$aksprodgroupdemo = "c5df26ef-bb43-406d-92f5-09dc0d7ebef6" # aks-prod-group-demo
+$teamabcpim = "ab41503f-3eaa-45bb-8876-59a8c344d762" # team-abc-pim
+$teamabc = "5c4f1e8b-2d6e-4c72-abf8-5ab15c32a326" # team-abc
 $startTime = Get-Date
 $endTime = $startTime.AddMonths(12).AddDays(-1)
 
 $params = @{
-  groupId = $teamDemo
-  principalId = $aksprodgroupdemo
+  groupId = $teamabcpim
+  principalId = $teamabc
   accessId = "member"
   principalType = "Group"
   action = "AdminAssign"
-  scheduleInfo    = @{
+  scheduleInfo     = @{
     startDateTime  = $startTime.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-    expiration    = @{
-                    type = "AfterDateTime"
-                    endDateTime = $endTime.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
-                  }
+    expiration     = @{
+      type = "noExpiration"
+    }
   }
   justification = "Entra ID - PIM Group Assignment"
 }
