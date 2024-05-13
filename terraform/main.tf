89f5a89053b52b8b5745e2be493ea122f85e5110
Fredrik Klingenberg <fredrkl@gmail.com>
2024-05-13 10:31:10 +0200

    ✨(tf): remove pim group
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,11 @@ resource "azuread_group" "main" {
   security_enabled = true
 }
 
-resource "azuread_group" "pim_group_demo" {
-  display_name     = "pim-group-tf-demo"
-  owners           = ["ea06710a-7380-4959-9cb8-f8cd0955ac59"]
-  security_enabled = true
-}
+#resource "azuread_group" "pim_group_demo" {
+#  display_name     = "pim-group-tf-demo"
+#  owners           = ["ea06710a-7380-4959-9cb8-f8cd0955ac59"]
+#  security_enabled = true
+#}
 
 resource "azuread_group" "pim_group_no_owner" {
   display_name     = "pim-group-tf-no_owner"
@@ -25,10 +25,10 @@ resource "azuread_group" "pim_group_no_owner" {
 
 # Starting with azuread group role management policy
 
-resource "azuread_group_role_management_policy" "example" {
-  group_id = azuread_group.pim_group_demo.id
-  role_id  = "member"
-}
+#resource "azuread_group_role_management_policy" "example" {
+#  group_id = azuread_group.pim_group_demo.id
+#  role_id  = "member"
+#}
 
 terraform {
   required_version = ">= 1.7"
